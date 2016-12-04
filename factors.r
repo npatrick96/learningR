cef03bb18b2a63a6c7b14f3d78cd8cd0b36977c9
Patrick Niyongabo <npatrick96@users.noreply.github.com>
2016-12-04 11:08:30 -0600

    Compare elements of an ordered factor

--- factors.r
+++ factors.r
@@ -50,3 +50,12 @@ factor_speed_vector <- factor(speed_vector, ordered=TRUE, levels=c("slow", "fast
 factor_speed_vector
 summary(factor_speed_vector)
 
+# Compare elements of an ordered factor.
+# Factor value for second data analyst
+da2 <- factor_speed_vector[2]
+
+# Factor value for fifth data analyst
+da5 <- factor_speed_vector[5]
+
+# Is data analyst 2 faster than data analyst 5?
+da2>da5
